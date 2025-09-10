1ba0dc8fe26dbfc1f32686ce930ffc2dfeba4383
Khoi Nguyen <bob.k.nguyen@gmail.com>
2025-09-10 11:53:02 -0400

    merge

--- home-manager/nvim/init.lua
+++ home-manager/nvim/init.lua
@@ -1,81 +1,192 @@
--- VIM OPTIONS
+-- Leader
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
---prefs
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.cursorline = true
-vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 8
-vim.opt.signcolumn = "yes"
-
---indets
+-- Indent
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.smartindent = true
 
---visuals
-vim.opt.fillchars:append({ eob = " ", vert = "│" })
-vim.opt.termguicolors = true
-vim.opt.showmode = false
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
 
--- KEYBINDS
+-- [[ Setting options ]]
+-- See `:help vim.o`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
 
---here i configure my leaders
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-vim.keymap.set('n', '<Space>', '<Nop>', { silent = true })
-vim.keymap.set('v', '<Space>', '<Nop>', { silent = true })
+-- Make line numbers default
+vim.o.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+vim.o.relativenumber = true
 
---the REAL keybinds begin ;3
---totally not primogens keybinds btw
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.o.mouse = 'a'
 
---vim.api.nvim_set_keymap("n", "<leader>tf", "<Plug>PlenaryTestFile", { noremap = false, silent = false })
+-- Don't show the mode, since it's already in the status line
+vim.o.showmode = false
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-vim.keymap.set("n", "=ap", "ma=ap'a")
-vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
 
--- greatest remap ever
-vim.keymap.set("x", "<leader>rp", [["_dP]])
+-- Enable break indent
+vim.o.breakindent = true
 
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>yy", [["+yy]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]])
-vim.keymap.set({ "n", "v" }, "<leader>P", [["+P]])
+-- Save undo history
+vim.o.undofile = true
 
-vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
--- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
+-- Keep signcolumn on by default
+vim.o.signcolumn = 'yes'
 
-vim.keymap.set("n", "Q", "<nop>")
---vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
---vim.keymap.set("n", "<M-h>", "<cmd>silent !tmux-sessionizer -s 0 --vsplit<CR>")
---vim.keymap.set("n", "<M-H>", "<cmd>silent !tmux neww tmux-sessionizer -s 0<CR>")
---vim.keymap.set("n", "<leader>f", function()
---    require("conform").format({ bufnr = 0 })
---end)
+-- Decrease update time
+vim.o.updatetime = 250
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+-- Decrease mapped sequence wait time
+vim.o.timeoutlen = 300
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>xq", "<cmd>!chmod +x %<CR>", { silent = true })
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
 
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+--
+--  Notice listchars is set using `vim.opt` instead of `vim.o`.
+--  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
+--   See `:help lua-options`
+--   and `:help lua-options-guide`
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
---getting the config module (lazy & other addons)
-require("config")
+-- Preview substitutions live, as you type!
+vim.o.inccommand = 'split'
 
+-- Show which line your cursor is on
+vim.o.cursorline = true
 
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
 
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.o.confirm = true
+
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- TIP: Disable arrow keys in normal mode
+-- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
+-- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
+-- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
+-- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
+-- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end
+
+---@type vim.Option
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
+
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+-- NOTE: Here is where you install your plugins.
+require('lazy').setup({
+  { import = 'plugins' },
+}, {
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
+vim.cmd.colorscheme 'catppuccin-macchiato'
--- home-manager/nvim/lua/plugins/mini.lua
+++ home-manager/nvim/lua/plugins/mini.lua
@@ -1,96 +1,37 @@
-local astolfo =[[
-                                                                    
-                      ▓▓▓████▓▓▓▓                                   
-                  █████▓▓▓█▓▓▓████▓                                 
-                 ████▒▒▒▒▓▓▓▓▒▒▒▒██                                 
-                 ██▓▒▒▓▓▓▓███▓▓▒▒▒█                                 
-              ▓███▓████████████████████                             
-           ▒█████▓█████████████████▓███████                         
-          ▓██████▓███████████████████▓█████▒▒                       
-         ███████▓██▓▓████████▓███▓▓███▓███████                      
-       ▒████████▓██▓▓█▓▓████▓██▓▓█▓███▓████████                     
-         ██████▓▒▓▓  ▓▓▓▓███▓▓▓▓▓▓ ▓▓▓▒███████                      
-         ██████▓▒▒     ▓▓██▓▓█       ▒▒███████                      
-         █▓▓█▓█▓▒ ▓██▓ ▒▓▓███▓█▒▒ █▓▒ ▒████▓▓█       {Hey Master!}  
-           ▒██▓▓▓▒███▓▓███▓████▒▓▓███▒▓▓███         /               
-       ▒█    █▓▓▓▓███████████████████▓▓▓██                          
-   ▓█  ▒█    ▒▓▓▓▓▓███████████████▓▓▓▓▓▓▓▒                          
-   ▒██▒▒██   ▒▒▓▓▓▒████▒▒▒▒██▒█████▒▓▓▓▒▒▒                          
-     ▒████▓▓  ▒▓▒██▒▒▓███▓▓▓▓▓███▓▒███▒▒▒                           
-     ████████     █▒ ▒▒████████▒▒▒▒█▓                               
-     ████████▓ ▒▒▓▓▒ ▒  ▒██████  ▒▒ ▒▓▓▒                            
-      ████████  ▓▓▓▓▓▓ ████████  ▓▓▓▓▓▓▓  ▓                         
-         ████    ▓▓▓▒██▒▒█████ ████▒▓▓  ▒▓▓▓▒                       
-              ▓▓▓ ▓   ▓▓▓ ███ ▓▓▓▓  ▒▓▓ ▒▓▓▓▓▒                      
-       ▒▓▓▓▓▓▓▓▓▓ ▓▓▓ ▓▓▓▓   ▓▓▓▓▓ ▓▓▓▓ ▒▓▓▓▓▒                      
-         ▒▒▒▒▒▒▒▒  ▒▒ ████████████ ▒▒▒  ▒▒▒▒▒▒                      
-]]
-return {
-  {
-    'nvim-mini/mini.nvim',
-    version = false,
-    config = function ()
-      vim.keymap.set('n', 's', '<Nop>', { noremap = true })
-      require("mini.extra").setup()
-      require("mini.icons").setup()
-      require("mini.files").setup()
-      require("mini.notify").setup()
-      local starter = require("mini.starter")
-      starter.setup( {header = astolfo, footer = "",
-      items = {
-        starter.sections.builtin_actions(),
-        starter.sections.recent_files(5),
-        {name = "", action = ":lua MiniFiles.open()", section = "Recent files"},
-      },
-      content_hooks = {
-        starter.gen_hook.adding_bullet("➤ "),
-        starter.gen_hook.aligning("center", "center"),
-      }
-    } )
+return { -- Collection of various small independent plugins/modules
+  'echasnovski/mini.nvim',
+  config = function()
+    -- Better Around/Inside textobjects
+    --
+    -- Examples:
+    --  - va)  - [V]isually select [A]round [)]paren
+    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+    --  - ci'  - [C]hange [I]nside [']quote
+    require('mini.ai').setup { n_lines = 500 }
 
-    require("mini.indentscope").setup({
-      draw = {
-        animation = function()
-          return 1
-        end,
-      },
-      symbol = "│",
-    })
-    require("mini.git").setup()
-    
+    -- Add/delete/replace surroundings (brackets, quotes, etc.)
+    --
+    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+    -- - sd'   - [S]urround [D]elete [']quotes
+    -- - sr)'  - [S]urround [R]eplace [)] [']
+    require('mini.surround').setup()
 
+    -- Simple and easy statusline.
+    --  You could remove this setup call if you don't like it,
+    --  and try some other statusline plugin
+    local statusline = require 'mini.statusline'
+    -- set use_icons to true if you have a Nerd Font
+    statusline.setup { use_icons = vim.g.have_nerd_font }
 
-     require("mini.statusline").setup({
-       content = {
-         active = function()
-          local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-           local git           = MiniStatusline.section_git({ trunc_width = 40 })
-           local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
-           local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
-           local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
-           local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 40, icon = ""})
-    
-           return MiniStatusline.combine_groups({
-             { hl = mode_hl,                 strings = { mode } },
-             { hl = 'MiniStatuslineDevinfo', strings = { git, diagnostics } },
-             '%<', -- Mark general truncate point
-             { hl = 'MiniStatuslineFilename', strings = { filename } },
-             '%=', -- End left alignment
-             { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-             { hl = mode_hl,                  strings = { search } },
-           })
-         end,
-         inactive = nil,
-       },
-       use_icons = true;
-     })
-     require("mini.surround").setup({ mappings = { add = 's'}})
-    require("mini.comment").setup({ mappings = { comment = 'sc', comment_line = 'scc', comment_visual = 'sc', textobject = 'sc' } })
-    require("mini.splitjoin").setup({ mappings = { toggle = 'sS'} })
+    -- You can configure sections in the statusline by overriding their
+    -- default behavior. For example, here we set the section for
+    -- cursor location to LINE:COLUMN
+    ---@diagnostic disable-next-line: duplicate-set-field
+    statusline.section_location = function()
+      return '%2l:%-2v'
+    end
 
-    vim.keymap.set("n", "<leader>e", function ()
-      MiniFiles.open()
-    end) 
-  end
-}
+    -- ... and there is more!
+    --  Check out: https://github.com/echasnovski/mini.nvim
+  end,
 }
