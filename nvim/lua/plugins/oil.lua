f575a3663bb908771d7794a9664bb42ecb2d1a11
Khoi Nguyen <bob.k.nguyen@gmail.com>
2025-09-10 17:59:39 -0400

    change stuff
    Signed-off-by: Khoi Nguyen <bob.k.nguyen@gmail.com>

--- nvim/lua/plugins/oil.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'stevearc/oil.nvim',
-  opts = {},
-  lazy = false,
-  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-  keys = {
-    { '<leader>o', '<cmd>Oil<cr>', desc = 'Oil' },
-  },
-}
